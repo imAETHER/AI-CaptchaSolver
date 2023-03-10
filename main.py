dfe1390b5470464552dd4798307f145d23f0940f
imAETHER <36291026+imAETHER@users.noreply.github.com>
2023-03-10 01:56:41 -0300

    FastAPI & Custom color filter

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-from flask import Flask, jsonify, request
-from solver import checkUrl, solveCaptcha
+from httpx import Client
+from fastapi import FastAPI
+from typing import Union
+from fastapi.responses import JSONResponse
+from solver import solveCaptcha
 
-app = Flask(__name__)
+api = FastAPI(title="CaptchaSolver")
+checker = Client(headers={
+  "User-Agent": "CapSolver v1.0"
+})
 
-@app.route('/solve', methods = ['GET'])
-def solverApi():
-  capUrl = request.args.get('captcha')
+@api.get("/api/captcha/solve")
+@api.get("/api/solve")
+def solve_api(captcha: str, color: Union[str, None] = None):
+  if (not str(captcha).startswith("https://cdn.discordapp.com")) or (not checker.get(captcha).status_code == 200):
+    return JSONResponse({ "success": False, "message": "INVALID_URL" })
+  
+  if color is None:
+    data = solveCaptcha(captcha)
+  else:
+    data = solveCaptcha(captcha, color=color)
 
-  if not checkUrl(capUrl):
-    print('Invalid Url: ' + capUrl)
-    return jsonify({ 'success': False, 'message': 'Invalid captcha URL provided.' })
+  if data is None or len(data) < 3:
+    return JSONResponse({ "success": False, "message": "SOLVE_FAILED" })
+  
+  return JSONResponse({ "success": True, "solve": data })
 
-  data = solveCaptcha(capUrl)
-  if data is None:
-    return jsonify({ 'success': False, 'message': 'Captcha solve failed!' })
-
-  return jsonify({ 'success': True, 'solve': data })
-
-if __name__ == '__main__':
-  app.run(debug = False, use_evalex=False, port=3007)
+if __name__ == "__main__":
+  import os
+  os.system("uvicorn main:api --host 0.0.0.0 --port 3007")
--- solver.py
+++ solver.py
@@ -10,44 +10,33 @@ model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5/best.pt')
 print(Fore.GREEN + 'Model loaded')
 
 # Removes the everything but the actual characters
-def process(img, color = (255, 0, 0), tolerance = 10):
+def process(img, hex_color, tolerance = 20):
   image_data = img.load()
   height, width = img.size
-  r_min, r_max = max(0, color[0] - tolerance), min(255, color[0] + tolerance)
-  g_min, g_max = max(0, color[1] - tolerance), min(255, color[1] + tolerance)
-  b_min, b_max = max(0, color[2] - tolerance), min(255, color[2] + tolerance)
+  r, g, b = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4)) #hex to rgb
+  r_min, r_max = max(0, r - tolerance), min(255, r + tolerance)
+  g_min, g_max = max(0, g - tolerance), min(255, g + tolerance)
+  b_min, b_max = max(0, b - tolerance), min(255, b + tolerance)
   for loop1 in range(height):
     for loop2 in range(width):
-      pixel = image_data[loop1, loop2]
-      if len(pixel) == 4:
-          r, g, b, _ = pixel
-          if not (r_min <= r <= r_max and g_min <= g <= g_max and b_min <= b <= b_max):
-            image_data[loop1, loop2] = 0, 0, 0, 0
-      else:
-          r, g, b = pixel
-          if not (r_min <= r <= r_max and g_min <= g <= g_max and b_min <= b <= b_max):
-            image_data[loop1, loop2] = 0, 0, 0
+      try:
+        pixel_r, pixel_g, pixel_b, _ = image_data[loop1, loop2]
+      except ValueError:
+        pixel_r, pixel_g, pixel_b = image_data[loop1, loop2]
+      if not (r_min <= pixel_r <= r_max and g_min <= pixel_g <= g_max and b_min <= pixel_b <= b_max):
+        image_data[loop1, loop2] = 0, 0, 0, 0
   return img
 
-def checkUrl(url) -> bool:
-  if url is None:
-    return False
-
-  if not url.startswith("https://cdn.discordapp.com"):
-    return False
-
-  request = requests.get(url)
-  return request.status_code == 200
-
-def solveCaptcha(url) -> str:
+def solveCaptcha(url, color = None) -> str:
   try:
     img = Image.open(requests.get(url, stream=True).raw)
-    img = process(img)
+    if color is not None:
+      img = prepare(img, color)
 
     result = model(img)
 
     a = result.pandas().xyxy[0].sort_values('xmin')
-    while len(a) > 6: # TODO: custom value for longer/shorter captchas
+    while len(a) > 6: #TODO: custom value for longer/shorter captchas
       lines = a.confidence
       linev = min(a.confidence)
       for line in lines.keys():
